0e1f2d8cf98f413d6273082b6b18e40b6c84733d
don-bi <dbi30@stuy.edu>
2021-09-23 18:42:38 -0400

    added return statements for the two methods

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,9 +5,11 @@ public class CtoF{
 
   public static double celsiusToFahrenheit(double celsius){
     fahrenheit = celsius * (9.0 / 5.0) + 32.0;
+    return fahrenheit;
   }
 
   public static double fahrenheitToCelsius(){
-    celsius = (fahrenheit - 32.0) * (5.0 / 9.0)
+    celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+    return celsius;
   }
 }
