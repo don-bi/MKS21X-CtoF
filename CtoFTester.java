00591ca142899a028ee3eb22fd200d49f25d724d
don-bi <dbi30@stuy.edu>
2021-09-23 18:44:25 -0400

    added print statements to main to test the methods

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,6 +1,9 @@
 public class CtoF{
   public static void main(String[] args){
-
+    System.out.println("celsiusToFahrenheit test:")
+    System.out.println(celsiusToFahrenheit(50.0))
+    System.out.println("fahrenheitToCelsius test:")
+    System.out.println(fahrenheitToCelsius(32.0))
   }
 
   public static double celsiusToFahrenheit(double celsius){
@@ -8,7 +11,7 @@ public class CtoF{
     return fahrenheit;
   }
 
-  public static double fahrenheitToCelsius(){
+  public static double fahrenheitToCelsius(double fahrenheit){
     celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
     return celsius;
   }
