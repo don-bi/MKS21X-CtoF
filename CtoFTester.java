ffb4d26ee7d9cf2ec5b1e2a30fe9c82cb1d4a206
don-bi <dbi30@stuy.edu>
2021-09-23 18:39:20 -0400

    added method names

---
+++ CtoFTester.java
@@ -0,0 +1,12 @@
+public class CtoF{
+  public static void main(String[] args){
+
+  }
+
+  public static double celsiusToFahrenheit(){
+  }
+
+  public static double fahrenheitToCelsius(){
+
+  }
+}
