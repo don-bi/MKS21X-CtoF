b77a6c4c15e83d665addc92622c487bc4d562152
don-bi <dbi30@stuy.edu>
2021-09-23 18:45:07 -0400

    added semicolons that I forgot

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,9 @@
 public class CtoF{
   public static void main(String[] args){
-    System.out.println("celsiusToFahrenheit test:")
-    System.out.println(celsiusToFahrenheit(50.0))
-    System.out.println("fahrenheitToCelsius test:")
-    System.out.println(fahrenheitToCelsius(32.0))
+    System.out.println("celsiusToFahrenheit test:");
+    System.out.println(celsiusToFahrenheit(50.0));
+    System.out.println("fahrenheitToCelsius test:");
+    System.out.println(fahrenheitToCelsius(32.0));
   }
 
   public static double celsiusToFahrenheit(double celsius){
